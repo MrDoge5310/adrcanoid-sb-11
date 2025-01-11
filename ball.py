116b274de82ff6e0f03169a95ef711befdb112fd
LOGIKA <danilltrotsenko2002@gmail.com>
2025-01-11 11:10:50 +0200

    1

--- ball.py
+++ ball.py
@@ -8,7 +8,7 @@ class Ball:
         self.size = 35
         self.rect = pygame.Rect(0, 0, self.size, self.size)
         self.rect.center = pos
-        self.speeds = [-2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2]
+        self.speeds = [-3, -2.5, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5, 3]
         self.dx = random.choice(self.speeds)
         self.dy = -2
 
@@ -22,10 +22,12 @@ class Ball:
         self.rect.center = x, y
 
         scr_width, scr_height = screen.get_size()
-        if self.rect.right >= scr_width or self.rect.x <= 0:
-            self.dx *= -1
+        if self.rect.right >= scr_width:
+            self.dx = -abs(self.dx)
+        elif self.rect.x <= 0:
+            self.dx = abs(self.dx)
         if self.rect.y <= 0:
-            self.dy *= -1
+            self.dy = abs(self.dy)
 
         if self.rect.colliderect(platform.rect):
             self.dx = random.choice(self.speeds)
